9f56603f04679ebb48eef3c7b283546ddf7a219b
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-08-31 14:43:37 +0530

    Time: 1 ms (99.89%) | Memory: 55.4 MB (66.52%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,13 +1,34 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        // HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        // for(int i =0;i<nums.length;i++){
+        //     int count = map.getOrDefault(nums[i],0)+1;
+        //     map.put(nums[i],count);
+        //     if(count>nums.length/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+
+        // boyer moore algo
+
+        int vote = 0;
+        int candiate = -1;
+
         for(int i =0;i<nums.length;i++){
-            int count = map.getOrDefault(nums[i],0)+1;
-            map.put(nums[i],count);
-            if(count>nums.length/2){
-                return nums[i];
+            if(vote == 0){
+                candiate = nums[i];
+                vote++;
             }
+            else if(candiate == nums[i]){
+                vote++;
+            }
+            else{
+                vote--;
+            }
+
         }
-        return -1;
+        return candiate;
     }
 }
