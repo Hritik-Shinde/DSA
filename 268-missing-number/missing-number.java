21cd99e7afe51f628ae2f471611b2153afe040da
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-01 13:29:51 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (49.04%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
         // Hashset
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i =0;i<nums.length;i++){
-            hs.add(nums[i]);
-        }
-        for(int i =0;i<nums.length;i++){
-            if(!hs.contains(i))
-                return i;
-        }
-        return nums.length;
+        // HashSet<Integer> hs = new HashSet<>();
+        // for(int i =0;i<nums.length;i++){
+        //     hs.add(nums[i]);
+        // }
+        // for(int i =0;i<nums.length;i++){
+        //     if(!hs.contains(i))
+        //         return i;
+        // }
+        // return nums.length;
          
         // using sort 
 
@@ -20,6 +20,16 @@ class Solution {
         //     }
         // }
         // return nums.length;
+
+        //formula 
+        int n = nums.length;
+        long expectedSum = (long) n *(n +1)/2;
+        long actuallSum = 0;
+        for(int num: nums){
+            actuallSum += num;
+        }
+
+        return (int) (expectedSum - actuallSum);
 
     }
 }
