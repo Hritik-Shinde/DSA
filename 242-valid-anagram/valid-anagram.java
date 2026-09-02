0b3b96d7d6a1caa0122d1dccf54549db20036e3e
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-02 13:37:18 +0530

    Time: 4 ms (83.53%) | Memory: 43.7 MB (99.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -12,26 +12,45 @@ class Solution {
 
         // Hashmap 
 
-        HashMap<Character,Integer> freq = new HashMap<>();
+        // HashMap<Character,Integer> freq = new HashMap<>();
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // for(int i =0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     freq.put(ch,freq.getOrDefault(ch,0)+1);
+        // }
+
+        // for(int j =0;j<t.length();j++){
+        //     char ch = t.charAt(j);
+        //     if(!freq.containsKey(ch)){
+        //         return false;
+        //     }
+        //         freq.put(ch,freq.get(ch)-1);
+        //         if(freq.get(ch)< 0){
+        //             return false;
+        //         }
+            
+        // }
+        // return true;
+
+        // asci method oly will small characher
+
+        int[] ans = new int[26];
+
         if(s.length() != t.length()){
             return false;
         }
         for(int i =0;i<s.length();i++){
-            char ch = s.charAt(i);
-            freq.put(ch,freq.getOrDefault(ch,0)+1);
-        }
+            ans[s.charAt(i) - 'a']++;
+        } 
 
-        for(int j =0;j<t.length();j++){
-            char ch = t.charAt(j);
-            if(!freq.containsKey(ch)){
+          for(int i =0;i<s.length();i++){
+            ans[t.charAt(i) - 'a']--;
+            if(ans[t.charAt(i) - 'a'] < 0){
                 return false;
             }
-                freq.put(ch,freq.get(ch)-1);
-                if(freq.get(ch)< 0){
-                    return false;
-                }
-            
-        }
+        } 
         return true;
     }
 }
