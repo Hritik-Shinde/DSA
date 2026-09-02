39b1ba8d4116dbf3d0b9cc9f8f907d831ca89f09
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-02 13:26:43 +0530

    Time: 17 ms (16.85%) | Memory: 46.6 MB (16.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         // sort methord 
-        char[] first = s.toCharArray();
-        char[] Second = t.toCharArray();
-        Arrays.sort(first);
-        Arrays.sort(Second);
-        if(Arrays.equals(first,Second)){
-            return true;
+        // char[] first = s.toCharArray();
+        // char[] Second = t.toCharArray();
+        // Arrays.sort(first);
+        // Arrays.sort(Second);
+        // if(Arrays.equals(first,Second)){
+        //     return true;
+        // }
+        // return false;
+
+        // Hashmap 
+
+        HashMap<Character,Integer> freq = new HashMap<>();
+        if(s.length() != t.length()){
+            return false;
         }
-        return false;
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        }
+
+        for(int j =0;j<t.length();j++){
+            char ch = t.charAt(j);
+            if(!freq.containsKey(ch)){
+                return false;
+            }
+                freq.put(ch,freq.get(ch)-1);
+                if(freq.get(ch)< 0){
+                    return false;
+                }
+            
+        }
+        return true;
     }
 }
