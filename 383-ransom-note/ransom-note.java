7139f0db96825becbd0a73c9222c2ddba899257c
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-02 16:31:29 +0530

    Time: 1 ms (99.95%) | Memory: 46.2 MB (79.42%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,26 +1,45 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> freq = new HashMap<>();
-        if(ransomNote.length() > magazine.length()){
+        // HashMap<Character,Integer> freq = new HashMap<>();
+        // if(ransomNote.length() > magazine.length()){
+        //     return false;
+        // }
+        // for(int i =0;i<magazine.length();i++){
+        //     char ch = magazine.charAt(i);
+        //     int count = freq.getOrDefault(ch,0)+1;
+        //     freq.put(ch,count);
+        // }
+
+        // for(int i =0;i<ransomNote.length();i++){
+        //     char ch = ransomNote.charAt(i);
+        //     if(!freq.containsKey(ch)){
+        //         return false;
+        //     }
+
+        //     freq.put(ch,freq.get(ch)-1);
+        //     if(freq.get(ch) < 0 ){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        int[] freq = new int[26];
+        if(ransomNote.length()>magazine.length()){
             return false;
         }
+
         for(int i =0;i<magazine.length();i++){
             char ch = magazine.charAt(i);
-            int count = freq.getOrDefault(ch,0)+1;
-            freq.put(ch,count);
+            freq[ch - 'a']++;
         }
-
         for(int i =0;i<ransomNote.length();i++){
             char ch = ransomNote.charAt(i);
-            if(!freq.containsKey(ch)){
-                return false;
-            }
-
-            freq.put(ch,freq.get(ch)-1);
-            if(freq.get(ch) < 0 ){
+            freq[ch - 'a']--;
+            if(freq[ch-'a']<0){
                 return false;
             }
         }
         return true;
     }
+ 
 }
