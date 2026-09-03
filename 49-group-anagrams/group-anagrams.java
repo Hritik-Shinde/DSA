7fe13a82e7169b1021e0fb8c2740d614d86a38e6
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-03 17:54:11 +0530

    Time: 6 ms (99.26%) | Memory: 50.1 MB (20.89%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        // hashmap solutions
+        HashMap<String,List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+
+            String key = new String(ch);
+            if(!map.containsKey(key)){
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(key,list);
+            } else{
+                map.get(key).add(str);
+            }
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
