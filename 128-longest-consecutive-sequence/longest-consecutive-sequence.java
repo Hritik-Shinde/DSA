ee4984def9dcdcd3d6d10f36f26bcf4ef15a44fe
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-06 23:08:12 +0530

    Time: 30 ms (74.05%) | Memory: 95.8 MB (15.80%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,22 +1,44 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int res =1 , curr = 1;
-        if(nums.length == 0){
-            return 0;
-        }
-        Arrays.sort(nums);
-        for(int i =1;i<nums.length;i++){
-            if(nums[i-1] +1 == nums[i]){
-                 curr++;
-            }
-            else if(nums[i-1] == nums[i]){
+        // int res =1 , curr = 1;
+        // if(nums.length == 0){
+        //     return 0;
+        // }
+        // Arrays.sort(nums);
+        // for(int i =1;i<nums.length;i++){
+        //     if(nums[i-1] +1 == nums[i]){
+        //          curr++;
+        //     }
+        //     else if(nums[i-1] == nums[i]){
               
-            }else{
-                curr = 1;
-            }
-            res = Math.max(res, curr);
+        //     }else{
+        //         curr = 1;
+        //     }
+        //     res = Math.max(res, curr);
+        // }
+
+        // return res;
+        
+        //HashSet
+
+        HashSet<Integer> set = new HashSet<>();
+        int ans = 0;
+        for(int num : nums){
+            set.add(num);
         }
 
-        return res;
+        for(int num: set){
+            if(!set.contains(num-1)){
+                int currentNum = num;
+                int currentSum = 1;
+                while(set.contains(currentNum+1)){
+                    currentNum ++;
+                    currentSum++;
+                }
+                ans = Math.max(ans,currentSum);
+            }
+            
+        }
+        return ans;
     }
 }
