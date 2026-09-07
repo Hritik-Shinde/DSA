1a063c90dd30563a1c39ff3a46a0c1ec478af40d
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-07 14:51:48 +0530

    Time: 2 ms (99.30%) | Memory: 44.2 MB (83.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,40 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String ns = s.toLowerCase();
-        ns = ns.replaceAll("[^a-z0-9]","");
-        if(ns.length() == 0){
-            return true;
-        }
-        int i =0,j = ns.length()-1;
-        char[] ch = ns.toCharArray();
+        // String ns = s.toLowerCase();
+        // ns = ns.replaceAll("[^a-z0-9]","");
+        // if(ns.length() == 0){
+        //     return true;
+        // }
+        // int i =0,j = ns.length()-1;
+        // char[] ch = ns.toCharArray();
+        // while(i<j){
+        //     if(ch[i] != ch[j]){
+        //         return false;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+
+        int i =0;
+        int j = s.length() -1;
         while(i<j){
-            if(ch[i] != ch[j]){
+            while(i<j &&!Character.isLetterOrDigit(s.charAt(i)))
+            {
+                i++;
+            }
+
+            while(i<j &&!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
                 return false;
             }
             i++;
             j--;
         }
+
         return true;
     }
     
