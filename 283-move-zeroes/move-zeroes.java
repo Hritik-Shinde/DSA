cf39388ae25a895c9de7905bdd3f1e5c9481d844
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-09 14:46:27 +0530

    Time: 2 ms (91.76%) | Memory: 47.8 MB (38.12%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -9,23 +9,35 @@ class Solution {
         //         }
         //     }
         // }
-        int i = 0;
-        int j = i+1;
-        while(i<j && j< nums.length){
-            if(nums[i] == 0 && nums[j] !=0 )
-            {
+        // 2 nd approch
+        // int i = 0;
+        // int j = i+1;
+        // while(i<j && j< nums.length){
+        //     if(nums[i] == 0 && nums[j] !=0 )
+        //     {
+        //         int temp = nums[i];
+        //         nums[i] = nums[j];
+        //         nums[j] = temp;
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums[i] != 0){
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums[j] == 0 && nums[i] == 0){
+        //         j++;
+        //     }
+        // }
+
+        // optimise 
+        int i =0;
+        for(int j =0;j<nums.length;j++){
+            if(nums[j] != 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
                 i++;
-                j++;
-            }
-            else if(nums[i] != 0){
-                i++;
-                j++;
-            }
-            else if(nums[j] == 0 && nums[i] == 0){
-                j++;
             }
         }
     }
