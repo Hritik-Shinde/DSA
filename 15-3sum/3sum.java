fdeae24d836f81abfc237718e068c2ecc5b66ee6
Hritik Shinde <51324464+Hritik-Shinde@users.noreply.github.com>
2026-09-12 20:34:17 +0530

    Time: 33 ms (70.77%) | Memory: 60.7 MB (5.10%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // HashSet<List<Integer>> set = new HashSet<>();
+        // for(int i =0;i<nums.length;i++){
+        //     for(int j =i+1;j<nums.length;j++){
+        //         for(int k = j+1;k<nums.length;k++){
+        //             if(nums[i]+nums[j]+nums[k] == 0){
+        //                 ArrayList<Integer> ans = new ArrayList<>();
+        //                 ans.add(nums[i]);
+        //                 ans.add(nums[j]);
+        //                 ans.add(nums[k]);
+        //                 Collections.sort(ans);
+        //                 set.add(ans);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return new ArrayList<>(set); tle
+        int sum =0;   
+        Arrays.sort(nums);
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length-2; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int left = i+1;
+            int right = nums.length -1;
+
+            while(left < right){
+                sum = nums[i] +nums[left]+nums[right];
+                if(sum == 0){
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    ans.add(list);
+                    left++;
+                    right--;
+
+                    while(left<right && nums[left-1] == nums[left] ){
+                        left++;
+                    }
+                    while(left<right && nums[right+1] == nums[right]){
+                        right--;
+                    }
+                }
+                else if(sum>0){
+                    right--;
+                }
+                else{
+                    left ++;
+                }
+            }
+
+        }
+        return ans;
+    }
+}
